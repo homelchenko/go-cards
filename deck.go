12185a491e94b03c1ca7313fadd9be205206dae7
Hennadii Omelchenko <hennadii@unity3d.com>
2019-03-10 14:36:35 +0200

    Generate a new deck of 52 cards

--- deck.go
+++ deck.go
@@ -4,6 +4,21 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
